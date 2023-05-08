f63e17034f167f9098afea04321f0a034e4a366c
高倉翔太 <takakurashouta@takakurashoutanoMacBook-Air.local>
2023-05-08 14:48:44 +0900

    PidGain.hとPidGian.cpp

---
+++ takakura/PidGain.h
@@ -0,0 +1,13 @@
+#ifndef PID_GAIN_H
+#define PID_GAIN_H
+
+class PidGain{
+    public:
+    double kp:
+    double ki:
+    double kd:
+
+    Pidgain(double _kp = 0.6, double _ki = 0.08, double _kd = 0.04);
+};
+
+#endif
